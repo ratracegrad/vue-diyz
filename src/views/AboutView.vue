7f7247214054114a7b9230ab20334b2e2be4ed72
Jennifer Bland <jennifer.bland@sbdinc.com>
2018-07-20 17:45:41 -0400

    Changed the tabs to have yellow underline and text when active

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <md-tabs md-sync-route md-alignment="fixed" class="md-transparent" >
-            <md-tab id="tab-team" md-label="Team" to="/about" >
-                <about-home></about-home>
-            </md-tab>
-            <md-tab id="tab-press" md-label="Press" to="/about/press" >
-                <router-view></router-view>
-            </md-tab>
-            <md-tab id="tab-faq" md-label="FAQ" to="/about/faq" >
-                <router-view></router-view>
-            </md-tab>
-        </md-tabs>
+        <md-theme md-name="yellow">
+            <md-tabs md-sync-route md-alignment="fixed" class="md-transparent" style="margin-bottom: 24px;">
+                <md-tab id="tab-team" md-label="Team" to="/about" style="padding: 0;">
+                    <about-home></about-home>
+                </md-tab>
+                <md-tab id="tab-press" md-label="Press" to="/about/press"  style="padding: 0;">
+                    <router-view></router-view>
+                </md-tab>
+                <md-tab id="tab-faq" md-label="FAQ" to="/about/faq"  style="padding: 0;">
+                    <router-view></router-view>
+                </md-tab>
+            </md-tabs>
+        </md-theme>
     </div>
 </template>
 
@@ -25,14 +27,11 @@ export default {
 };
 </script>
 
-<style >
-.md-tabs {
+<style lang="scss">
+.md-tabs-navigation {
     margin-bottom: 24px;
 }
-.md-tab {
-    padding: 0;
-}
-.md-tabs.md-alignment-fixed .md-tabs-navigation {
-    justify-content: space-between;
+:root {
+    --md-theme-default-primary-on-background: #ffd100;
 }
 </style>
