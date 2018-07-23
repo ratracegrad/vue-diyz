f2118234d4658c1c25901f041b0c49b0deaa53a6
Jennifer Bland <jennifer.bland@sbdinc.com>
2018-07-23 19:01:21 -0400

    updated carousel so that it dynamically resizes

--- src/components/HeaderCarousel.vue
+++ src/components/HeaderCarousel.vue
@@ -1,12 +1,11 @@
 <template>
-    <v-carousel>
+    <v-carousel :style="{ backgroundColor: 'lightgray', height: height, backgroundSize: 'cover' }">
         <v-carousel-item
                 v-for="(item,i) in items"
                 :key="i"
                 :lazy="true"
                 :src="item.image"
                 :interval="4000"
-                :style="{ backgroundColor: 'lightgray', height: height, backgroundSize: 'cover' }"
         ></v-carousel-item>
     </v-carousel>
 </template>
@@ -40,11 +39,9 @@ export default {
                 return (Math.round(window.innerWidth) * 9) / 16 + 'px';
             } else if (window.innerWidth >= 1060) {
                 return '450px';
-            } else if (this.querySelector('.header-picture')) {
+            } else if (this.querySelector('.v-carousel')) {
                 return (
-                    (Math.round(
-                        this.querySelector('.header-picture').offsetWidth
-                    ) *
+                    (Math.round(this.querySelector('.v-carousel').offsetWidth) *
                         9) /
                         16 +
                     'px'
